0fa344e7658cc8796df2b320daa1b6889954f3ae
ismael diallo <dialloismael439@gmail.com>
2022-04-11 17:59:13 +0000

    pet clinic files

---
+++ petclinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+    public Vet(String firstname, String lastname) {
+        super(firstname, lastname);
+    }
+}
---
+++ petclinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic;
+
+//import guru.springframework.sfgpetclinic.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+ SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
